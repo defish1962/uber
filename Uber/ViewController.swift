a0851b548f3fbeb8abca7761678e4b87809a715d
David Fish <david.fish@toptal.com>
2021-02-18 16:55:19 -0700

    Initial commit

--- Uber/ViewController.swift
+++ Uber/ViewController.swift
@@ -7,19 +7,97 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var driverLabel: UILabel!
+    @IBOutlet weak var riderLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var riderDriverSwitch: UISwitch!
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var bottomButton: UIButton!
+    
+    var signUpMode = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func topTapped(_ sender: Any) {
+        if emailTextfield.text == "" || passwordTextField.text == "" {
+            displayAlert(title: "Missing Information", message: "You must provide both a email and password")
+        } else {
+            if let email = emailTextfield.text {
+                if let password = passwordTextField.text {
+                    if signUpMode {
+                        // SIGN UP
+                        FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                            if error != nil {
+                                self.displayAlert(title: "Error", message: error!.localizedDescription)
+                            } else {
+                                
+                                if self.riderDriverSwitch.isOn {
+                                    // DRIVER
+                                    let req = FIRAuth.auth()?.currentUser?.profileChangeRequest()
+                                    req?.displayName = "Driver"
+                                    req?.commitChanges(completion: nil)
+                                    self.performSegue(withIdentifier: "driverSegue", sender: nil)
+                                } else {
+                                    // RIDER
+                                    let req = FIRAuth.auth()?.currentUser?.profileChangeRequest()
+                                    req?.displayName = "Rider"
+                                    req?.commitChanges(completion: nil)
+                                    self.performSegue(withIdentifier: "riderSegue", sender: nil)
+                                }
+                            }
+                        })
+                    } else {
+                        // LOG IN
+                        FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                            if error != nil {
+                                self.displayAlert(title: "Error", message: error!.localizedDescription)
+                            } else {
+                                if user?.displayName == "Driver" {
+                                    // DRIVER
+                                    self.performSegue(withIdentifier: "driverSegue", sender: nil)
+                                } else {
+                                    // RIDER
+                                    self.performSegue(withIdentifier: "riderSegue", sender: nil)
+                                }
+                            }
+                        })
+                    }
+                }
+            }
+        }
     }
-
-
+    
+    func displayAlert(title:String, message:String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func bottomTapped(_ sender: Any) {
+        if signUpMode {
+            topButton.setTitle("Log In", for: .normal)
+            bottomButton.setTitle("Switch to Sign Up", for: .normal)
+            riderLabel.isHidden = true
+            driverLabel.isHidden = true
+            riderDriverSwitch.isHidden = true
+            signUpMode = false
+        } else {
+            topButton.setTitle("Sign Up", for: .normal)
+            bottomButton.setTitle("Switch to Log In", for: .normal)
+            riderLabel.isHidden = false
+            driverLabel.isHidden = false
+            riderDriverSwitch.isHidden = false
+            signUpMode = true
+        }
+    }
+    
 }
 
